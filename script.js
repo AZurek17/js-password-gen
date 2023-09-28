1991fc11e6d6b8423d5f04758f19d2e78cd110b7
Andy <84168944+AZurek17@users.noreply.github.com>
2023-09-28 15:39:11 -0700

    finialized conditional statement for prompts

--- script.js
+++ script.js
@@ -22,43 +22,115 @@ var minLength = 8;
 var maxLength = 128;
 
 function generatePassword() {
-  var passwordLength = prompt("Pick a number between " + minLength + ", and " + maxLength); 
+  var passwordLength = prompt("How many charaters long? Pick a number between " + minLength + ", and " + maxLength); 
 // if charaters under 8 and over 128 characters, then invalid response
      if (passwordLength < minLength || passwordLength > maxLength) { 
-      confirm("Pick a number between  " + minLength + " - " + maxLength);
-     // generatePassword();
-    }
-      else { 
-      var lowerCase = confirm("Do you want to use lowercase characters?");
-      var upperCase = confirm("Do you want to use uppercase characters?");
-      var numCase = confirm("Do you want to use numbers characters?");
-      var specialCase = confirm("Do you want to use special characters?");
-      }
-
-      if (!lowerCase && !upperCase && !numCase && !specialCase); {
-      alert("You must chose one charcter type");
-      alert("Let's start over");
+      alert("Pick a number between  " + minLength + " - " + maxLength);
       generatePassword();
-      }
-    }
+     }
+     else {
+     var lowerCase = confirm("Do you want to use lowercase characters? OK = YES, Cancel = NO");
+     console.log(lowerCase);
+     var upperCase = confirm("Do you want to use uppercase characters? OK = YES Cancel = NO");
+     console.log(upperCase);
+     var numCase = confirm("Do you want to use numeric characters? OK = YES Cancel = NO");
+     console.log(numCase);
+     var specialCase = confirm("Do you want to use special characters? OK = YES Cancel = NO");
+     console.log(specialCase);
+     charCase();
 
-      var lowerCaseChar = ("abcdefghijklmnopqustuvwxyz");
-      var upperCaseChar = ("ABCDEFGHIJKLMNOPQURSTVWYZ");
-      var numCaseCha = ("0123456789");
-      var specialCaseCha = ("!@#$%^&*()_+");
+    function charCase() {
+      if(lowerCase == upperCase == numCase == specialCase) {
+        alert("YOU MUST CHOOSE AT LEAST ONE CHARACTER TYPE!!!!!");
+        alert("Lets try again");
+        generatePassword();
+         }
+        }
      
+    }
+  }
+
+
+  function randomPassword() {
       
+    lowerstr ["abcdefghijklmnopqustuvwxyz"] = Math.floor(Math.random()*35);
+    console.log(lowerCaseChar(35));
+    Upperstr ["ABCDEFGHIJKLMNOPQURSTVWYZ"] = Math.floor(Math.random()*35);
+    console.log(lowerCaseChar(35));
+    numstr [numCaseaseChar] = Math.floor(Math.random()*35);
+    console.log(lowerCaseChar(35));
+    specialstr [specialCaseCaseChar] = Math.floor(Math.random()*35);
+    console.log(lowerCaseChar(35));
+  }
+    //  if (charSelc = false){
+    //     charSelc()
+
+
+      
+      
+      
+      
+      //lowerCase===false || upperCase===false || numCase === false || specialCase === false) {
+      
+      //
+      //generatePassword();
+    
+  
+
+      // else { 
+      //   var lowerCase = confirm("Do you want to use lowercase characters?")
+      //   var upperCase = confirm("Do you want to use uppercase characters?");
+      //   var numCase = confirm("Do you want to use numeric characters?");
+      //   var specialCase = confirm("Do you want to use special characters?");
+      // }
+
+                
+      
+       
+
+ generatePassword() 
+ //randomPassword()   
+
+/*lowerCaseChar//
+    var lowerCaseChar = ["abcdefghijklmnopqustuvwxyz"];
+    var upperCaseChar = ["ABCDEFGHIJKLMNOPQURSTVWYZ"];
+    var numCaseChar = ["0123456789"];
+    var specialCaseChar = ["!@#$%^&*()_+"];
+    
+    //
+  
+    //randomPassword()
+  
+      //if (isNAN(generatePassword))
+      //console.log(randomPasswod(3)
+      //   Math.random() .toString(2).lowerCaseChar +
+      //   Math.random() .toString(2).upperCaseChar +
+      //   Math.random() .toString(2).lowerCaseChar +
+      //   Math.random() .toString(2).upperCaseChar);
+      //   //.toUpperCase() .slice (2)
+        //.toLowerCase() .slice (4);
+        //.numCaseChar() .slice (0)
+        //.specialCaseChar() .splice (6));
+        // .upperCaseChar() .slice (4)+
+        // .numCaseChar() .slice (6)+
+        // .specialCaseChar() .splice (8)+;
+    
      
+     
+  // add above later
+      
+    
+  
+  //generatePassword();
+  //randomPassword();
+    //for (var 1 =0; i<passwordLength; i++) {
+
+    //console.log(Math.floor(Math.random() * 8 /* Charactervalue// */
+
     
 
    
 // prompt user with a questions about password charcters and store respones in variables
-    
- 
-  
-generatePassword()
-  
-
 // if user does not choose "ok" on all, alert "must choose at least one"
 // store respones in variables
 // store uppercase letters in a variable - array
@@ -71,6 +143,3 @@ generatePassword()
 // in the loop push the string[random index] into empty array
 // once loop is completed, creaete string from the array
 // return password string
-
-
-//console.log(Math.floor(Math.random() * 8 /* Charactervalue// */)
