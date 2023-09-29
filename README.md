5694d483483509d8e7ec9e9658c56b765775e417
Andy <84168944+AZurek17@users.noreply.github.com>
2023-09-28 20:29:20 -0700

    added to READNE

--- README.md
+++ README.md
@@ -1,1 +1,88 @@
 # js-password-gen
+
+[Visit the Deployed Site](https://azurek17.github.io/js-password-gen/)
+
+## Technoloy used:
+* JavaScript - If...Else - https://www.w3schools.com/jsref/jsref_if.asp
+* JavaScript - Logical Operators - https://www.w3schools.com/java/java_operators.asp
+* JavaScript - For Loop - https://www.w3schools.com/java/java_for_loop.asp
+* JavaScript - Math.random() - https://www.w3schools.com/jsref/jsref_random.asp
+
+## Description
+A random password generator. Starter code was provide to modify to prompts a user with options to generates a strong random password automatically.
+
+## Usage
+
+When you go to the website, you are prompt to "Generate Password".  They the user clicks the "Generate Password" the following occurs:
+
+A dialog box pops up asking the user to pick a number between 8, and 128.  If the users pick a number less than 8 or over 128, and error pops up. When the user confirms, it loops to the start.  
+
+This was complished by using Prompts, Alerts, If Else statements with comparision operaters, For loop, and Math.randon (to generate password).  
+
+Below are a few examples of code:
+
+--------------------------------------------------------------------
+
+### Prompt:
+
+//javascript
+
+var passwordLength = prompt( "message goes here");
+
+//
+
+--------------------------------------------------------------------
+
+### Alert, Confirm, If Else, & comparison operator, and calling the function to restart:
+
+//javascript
+
+if (passwordLength < minLength || passwordLength > maxLength) { 
+      alert("ERROR - Pick a number between  " + minLength + " - " + maxLength);
+      generatePassword();
+     }
+
+     else {
+     var lowerCase = confirm(" add text here");
+     var upperCase = confirm("add text here");
+     var numCase = confirm("add text here");
+     var specialCase = confirm("add text here");
+     }
+//
+
+---------------------------------------------------------------------
+
+### For Loop, Math.Random 
+
+//javascript 
+
+  
+    var charSel = ""; 
+    var trueValue = [];
+
+    if (lowerCase) {
+      charSel = charSel.concat(lowerChar); console.log();
+    }
+    if (upperChar) {
+      charSel = charSel.concat(upperChar); console.log();
+    }
+    
+    for (var i =0; i < passwordLength; i++){
+    trueValue.push(charSel[Math.floor(Math.random() * charSel.length)]);
+    }
+    password = trueValue.join ("");
+    return password;
+
+//
+
+------------------------------------------------------
+
+
+
+## Credits
+* stater code provided: https://github.com/coding-boot-camp/friendly-parakeet
+* Tutoring Session
+
+
+----------------------------------------------------------------------------------
+© 2023 github.com/AZurek17/js-password-gen Confidential and Proprietary. All Rights Reserved.
--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function generatePassword() {
   var passwordLength = prompt("How many charaters long? Pick a number between " + minLength + ", and " + maxLength); 
 // if charaters under 8 and over 128 characters, then invalid response
      if (passwordLength < minLength || passwordLength > maxLength) { 
-      alert("Pick a number between  " + minLength + " - " + maxLength);
+      alert("ERROR - Pick a number between  " + minLength + " - " + maxLength);
       generatePassword();
      }
      // setting character options to true or false
@@ -38,7 +38,7 @@ function generatePassword() {
 
     // created empty vars 
     var charSel = ""; 
-    var trueVaule = [];
+    var trueValue = [];
 
     // using String Method with concat, to join two or more strings and set emtpy var charSel
     if (lowerCase) {
@@ -55,10 +55,10 @@ function generatePassword() {
     }
     // creating for loop for string and setting empty var vauleTrue 
     for (var i =0; i < passwordLength; i++){
-    vauleTrue.push(charSel[Math.floor(Math.random() * charSel.length)]);
+    trueValue.push(charSel[Math.floor(Math.random() * charSel.length)]);
     }
     // setting emty var to creating for loop for string
-    password = trueVaule.join ("");
+    password = trueValue.join ("");
     return password;
 }
 }
@@ -77,7 +77,60 @@ function writePassword() {
   passwordText.value = password;
 }
 
- 
 
 
- 
+
+ //randomPassword()   
+
+/*lowerCaseChar//
+    var lowerCaseChar = ["abcdefghijklmnopqustuvwxyz"];
+    var upperCaseChar = ["ABCDEFGHIJKLMNOPQURSTVWYZ"];
+    var numCaseChar = ["0123456789"];
+    var specialCaseChar = ["!@#$%^&*()_+"];
+    
+    //
+  
+    //randomPassword()
+  
+      //if (isNAN(generatePassword))
+      //console.log(randomPasswod(3)
+      //   Math.random() .toString(2).lowerCaseChar +
+      //   Math.random() .toString(2).upperCaseChar +
+      //   Math.random() .toString(2).lowerCaseChar +
+      //   Math.random() .toString(2).upperCaseChar);
+      //   //.toUpperCase() .slice (2)
+        //.toLowerCase() .slice (4);
+        //.numCaseChar() .slice (0)
+        //.specialCaseChar() .splice (6));
+        // .upperCaseChar() .slice (4)+
+        // .numCaseChar() .slice (6)+
+        // .specialCaseChar() .splice (8)+;
+    
+     
+     
+  // add above later
+      
+    
+  
+  //generatePassword();
+  //randomPassword();
+    
+
+    //console.log(Math.floor(Math.random() * 8 /* Charactervalue// */
+
+    
+
+   
+// prompt user with a questions about password charcters and store respones in variables
+// if user does not choose "ok" on all, alert "must choose at least one"
+// store respones in variables
+// store uppercase letters in a variable - array
+// store lowercase letters in a variable - array
+// store numbers in a variable - array
+// store special characters in a variable -  array
+// declare an empty string
+// combine predefined stored values that can be in the password in a string
+// create a for loop, with users charater amount as the number of max characters select the loop wiill execute
+// in the loop push the string[random index] into empty array
+// once loop is completed, creaete string from the array
+// return password string
